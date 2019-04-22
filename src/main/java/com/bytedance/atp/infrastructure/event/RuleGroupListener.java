b6a4b51c9e480ca430c2cd7b6a7328bd87e8d785
piemonj <jiayu8299786>
2019-04-22 18:15:13 +0800

    # Newer

---
+++ src/main/java/com/bytedance/atp/infrastructure/event/RuleGroupListener.java
@@ -0,0 +1,52 @@
+package com.bytedance.atp.infrastructure.event;
+
+import com.bytedance.atp.application.ConfigCenterApplicationService;
+import com.bytedance.atp.application.ReportingApplicationService;
+import com.bytedance.atp.common.Rule;
+import com.bytedance.atp.domain.model.group.event.RuleGroupChangedEvent;
+import com.bytedance.atp.domain.model.group.event.RuleGroupCreatedEvent;
+import com.bytedance.atp.domain.model.runtime.event.FlowStartedEvent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class RuleGroupListener {
+
+
+    //Spring上下文
+    public ApplicationEventPublisher bus;
+
+
+    @Autowired
+    private ConfigCenterApplicationService configCenterApplicationService;
+
+    @Async
+    @EventListener
+    public void onRuleGroupCreated(RuleGroupCreatedEvent event) {
+        //Invoke Report Rich Method
+        String gitlab = event.getGitlab();
+        List<Rule> rules = event.getRules();
+        String ruleGroupId = event.getRuleGroupId();
+
+        configCenterApplicationService.configCenterIniter(ruleGroupId,rules);
+
+    }
+
+
+    @Async
+    @EventListener
+    public void onRuleGroupChanged(RuleGroupChangedEvent event) {
+        //Invoke Report Rich Method
+        String gitlab = event.getGitlab();
+        List<Rule> rules = event.getRules();
+        String ruleGroupId = event.getRuleGroupId();
+
+        configCenterApplicationService.configCenterChanger(ruleGroupId,rules);
+
+    }
+}
